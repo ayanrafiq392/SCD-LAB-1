ccd6b03c6c2a14b171e68eec87a63766f8687991
ayanrafiq392 <ayaanrafiq76@gmail.com>
2025-10-28 04:15:20 +0500

    Update myjavaproject.java

--- main.java/src/main/java/myjavaproject.java
+++ main.java/src/main/java/myjavaproject.java
@@ -3,16 +3,10 @@ package main.java;
 public class myjavaproject{
 
 	public static void main(String[] args) {
-		System.out.println("ayan");
-		System.out.println("sir syed university");
-		System.out.println("helooo world");
-		System.out.println("Now i am trying pull option");
-	
-		// updating the feature to try merge option
-
-		// now doing pull and merge option
+		System.out.println("Tthis is new branch update feature ");
 	}
 
 }
+
 
 
