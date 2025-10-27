1b5d69e135a606f3c0d3b27d63ab53dc0f9082fe
ayanrafiq392 <ayaanrafiq76@gmail.com>
2025-10-28 03:06:30 +0500

    Update myjavaproject.java

--- main.java/src/main/java/myjavaproject.java
+++ main.java/src/main/java/myjavaproject.java
@@ -6,6 +6,9 @@ public class myjavaproject{
 		System.out.println("ayan");
 		System.out.println("sir syed university");
 		System.out.println("helooo world");
+		System.out.println("Now i am trying pull option");
+	
 	}
 
 }
+
