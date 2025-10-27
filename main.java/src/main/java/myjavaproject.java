6ed0cb80604efd4f105fa2803c3b517968ddc833
USER <USER@AyanRafiq>
2025-10-28 01:57:40 +0500

    first comit

---
+++ main.java/src/main/java/myjavaproject.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class myjavaproject{
+
+	public static void main(String[] args) {
+		System.out.println("ayan");
+		System.out.println("sir syed university");
+
+	}
+
+}
